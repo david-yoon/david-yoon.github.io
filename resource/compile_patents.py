b5f668f33734303374c4d9a9ab48618870fe57db
david yoon <syoon@adobe.com>
2026-01-22 11:16:49 -0800

    .

--- resource/compile_patents.py
+++ resource/compile_patents.py
@@ -5,25 +5,11 @@ cur = pathlib.Path().resolve()
 # df = pd.read_csv(str(cur) + "/resource/data_patents.csv")
 df = pd.read_csv(str(cur) + "/data_patents.csv")
 
-# Count patents per type
-type_counts = {}
-for index, row in df.iterrows():
-    try:
-        t = row["Type"]
-        type_counts[t] = type_counts.get(t, 0) + 1
-    except:
-        continue
-
 type_patent = ""
 item = ""
 body = ""
 body += '''<hr>
 <h3 style="color: #2c3e50; border-bottom: 2px solid #3498db; padding-bottom: 5px; display: inline-block;">Patents</h3>
-<style>
-  ol.patents-list { list-style: none; }
-  ol.patents-list li { counter-increment: patent-item -1; }
-  ol.patents-list li::before { content: "[" counter(patent-item) "] "; }
-</style>
 <div style="max-height: 500px; overflow-y: auto; padding-right: 10px; border: 1px solid #e0e0e0; border-radius: 5px; background: #fafafa;">
 '''
 
@@ -37,7 +23,6 @@ for index, row in df.iterrows():
                 item += "</ol>\n"
             
             type_patent = row["Type"]
-            cnt = type_counts.get(type_patent, 0)
                     
             item += "<font color=lightblue>\n"
             
@@ -45,10 +30,10 @@ for index, row in df.iterrows():
             
             # item += "  <h4>[ {} ]</h4>\n".format(row["Type"])
             item += "</font>\n"
-            item += "<ol class=\"patents-list\" style=\"line-height:1.4em; counter-reset: patent-item {};\">\n".format(cnt + 1)
+            item += "<ol reversed style=\"line-height:1.4em\">\n"
             item += "<font size=\"2\">"
     
-        item += "\t<li style=\"margin-left: 0px;\">\n"
+        item += "\t<li style=\"margin-left: 15px;\">\n"
     
         # add issue
         if str(row["issued"]) == "1":
